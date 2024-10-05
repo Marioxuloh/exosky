844e9e81e876390c880f56e4aae3e82f8b4b405f
searchfounders <searchfounders@protonmail.com>
2024-10-05 15:08:45 +0200

    add: setting to deploy on all interfaces

--- ExoskyBackEnd/ExoskyBackEnd/settings.py
+++ ExoskyBackEnd/ExoskyBackEnd/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-9$k+ef%&$jwceq-d-3uvxtt99kcq#5)%tl2+xlvp-y-c)1c+zm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
